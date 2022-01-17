daf6ae8400ca0ea48cc8834b014d0b358c69c3e9
jki503 <jki503@naver.com>
2022-01-17 15:17:36 +0900

    [INIT] startr the project for introSrping [ADD] section1, section2 for the source code becasue the lecture is very short..

---
+++ project/src/main/java/com/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,50 @@
+package com.hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!");
+        return "hello"; //resource -> templates -> hello.html로 랜더링.
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 body 부에 이 데이터를 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //json 방식으로 content 반환
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){ //프로퍼티 접근 방식
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+    }
+
+}
