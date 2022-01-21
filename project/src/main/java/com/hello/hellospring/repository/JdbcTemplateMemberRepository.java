956648fc7006ab4f746881827a92b9269bdbf834
jki503 <jki503@naver.com>
2022-01-21 16:03:16 +0900

    [ADD] the source code for section6 Repository with JDBC and JDBCTemplate

---
+++ project/src/main/java/com/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,66 @@
+package com.hello.hellospring.repository;
+
+import com.hello.hellospring.domain.Member;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        /**
+         * SimpleJdbcInsert : table 명이랑 pk명으로 Insert문 만들어진다.
+         * executeAndReturnKey로 key를 받고 그 키를 멤버의 setid로 만들어줌..
+         */
+
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+        Number key = jdbcInsert.executeAndReturnKey(new
+                MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        //Template method pattern이 많이 들어가있다..
+        List<Member> result = jdbcTemplate.query("select * from member where id =?", memberRowMapper(),id);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("select * from member where name =?", memberRowMapper(),name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from member ", memberRowMapper());
+    }
+
+    private RowMapper<Member> memberRowMapper(){
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
+    }
+}
---
+++ project/src/test/java/com/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,66 @@
+package com.hello.hellospring.service;
+
+import com.hello.hellospring.domain.Member;
+import com.hello.hellospring.repository.MemberRepository;
+import com.hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+/**
+ * DB는 Transaction 개념
+ * Commit을 해야 db에 들어감
+ * AutoCommit이냐 아니냐
+ * Test 끝난담에 롤백해버리면 DB 데이터 다 날림
+ * 그 롤백해주는 어노테이션이 @Transactional이 해줌
+ * 즉 test 끝나면 DB에 반영이 안되고 Rollback 시켜줌.
+  */
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void join() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("jung");
+
+        //when
+        Long SaveId = memberService.join(member);
+
+        //then
+
+        // the refactoring code not lecture code
+        memberRepository.findById(SaveId)
+                .ifPresent(m -> assertEquals(member.getName(),m.getName()));
+
+    }
+
+    @Test
+    public void duplicateMemberTest() throws Exception{
+        //given
+        Member member1 = new Member();
+        member1.setName("jung");
+        Member member2 = new Member();
+        member2.setName("jung");
+
+        //when
+        memberService.join(member1);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+}
