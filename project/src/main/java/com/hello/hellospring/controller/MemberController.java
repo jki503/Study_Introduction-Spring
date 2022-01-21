6902160c5fc5a4bd4ec13252e139be2dd6a788cc
jki503 <jki503@naver.com>
2022-01-21 15:37:05 +0900

    [ADD] the source code for section5 createMember and memberList about getMapping postMapping

--- project/src/main/java/com/hello/hellospring/controller/MemberController.java
+++ project/src/main/java/com/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.hello.hellospring.controller;
 
+import com.hello.hellospring.domain.Member;
 import com.hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 스프링이 뜰 때 컨트롤러를 들고 있음, 스프링 빈이 관리한다.
 public class MemberController {
@@ -12,5 +18,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList"; // 정적 페이지 요구
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; // redirection
     }
 }
